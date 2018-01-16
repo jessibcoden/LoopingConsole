6156ac46ac556ae279a64f3caaa64246aade4919
Jess <jessibcoden@gmail.com>
2018-01-15 20:54:57 -0600

    looping in class exercise

---
+++ LoopingConsole/Children.cs
@@ -0,0 +1,31 @@
+﻿//Do NOT delete until done writing class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole
+{
+    //POCO "Plain old C# Object" = class without any methods attached
+
+    //classes are implicitly public
+    class Child
+    {
+       //properties are implicitly private , the following format is considerd an "auto property"
+        public string Name { get; set;}
+        public bool Sick { get; set; }
+
+        public string ForgeASickNote()
+        {
+            if (Sick)
+            {
+                return $"Please excuse {Name} from class today..";
+            }
+
+            //else is implied:
+                return $"{Name}, you still have to go to shcool, quit faking. Love, Mom";
+        }
+
+    }
+}
---
+++ LoopingConsole/Program.cs
@@ -0,0 +1,76 @@
+﻿//depenentcies, greyed out if not using, delete them if not using them:
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole  
+{
+    //Classes are the blueprint
+    class Program
+    {
+        //void means that it doesn't return anything
+        static void Main(string[] args)
+        {
+            //List is a "generic type", and is used like an array
+            var children = new List<Child>
+            {
+                new Child {Name = "Sawyer", Sick = true},
+                new Child {Name = "Sage", Sick = false}
+            };
+
+
+            //instantiate copy of Child instance (class) / initialize object:
+            //var sawyer = new Child
+            //{
+            //    Name = "Sawyer",
+            //    Sick = true
+            //};
+
+            //var sage = new Child
+            //{
+            //    Name = "Sage",
+            //    Sick = false
+            //};
+
+            //add child to children list
+            //children.Add(sawyer);
+            //children.Add(sage);
+
+            //for (var i = 0; i < children.Count -1; i++)
+            //{
+            //    Console.WriteLine($" {child.Name} is {(child.Sick ? "sick" : "healthy")}");
+            //    Console.WriteLine(child.ForgeASickNote());
+            //}
+
+            ////a while loop will continue to loop as long as the statement is true:
+            //while (children.Count == 2)
+            //{
+            //    Console.WriteLine($" {child.Name} is {(child.Sick ? "sick" : "healthy")}");
+            //    Console.WriteLine(child.ForgeASickNote());
+            //}
+
+            foreach (var child in children)
+            {
+                if (child.Name == "Sawyer")
+                {
+                    //"continue" will stop the current itteration of the loop and continue with the next element in the list
+                    continue;
+                    //"break;" will stop the loop entirely
+                }
+                //string interpolation with conditional ternary:
+                Console.WriteLine($" {child.Name} is {(child.Sick ? "sick" : "healthy")}");
+                //call methods from class:
+                Console.WriteLine(child.ForgeASickNote());
+            }
+
+            Console.WriteLine("This is our second time going over the same creap");
+
+            //Console apps need to be stopped, ReadLine stops and waits, reads a string..
+            //will have to type something into the console to stop app
+            Console.ReadLine(); 
+
+        }
+    }
+}
